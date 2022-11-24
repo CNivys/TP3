009ec57051c95f30c5e3d29272751a65394ea263
CNivys <clement.frimin@uha.fr>
2022-11-24 09:21:51 +0100

    Exercice 4

---
+++ Exercice 4/Factiorelle.py
@@ -0,0 +1,17 @@
+n = int(input("Entrez un nombre à factorialiser :"))
+type = input("Saisir une boucle for ou while")
+if type == "for":
+    fact = 1
+    for i in range(1, n+1):
+        fact = fact * i
+        print(fact)
+    print("{}! = {}".format(n, fact))
+elif type == "while":
+    fact = 1
+    nb = n
+    while nb>= 1:
+        fact = fact * nb
+        nb = nb - 1
+        print(fact)
+    print("{}! = {}".format(n,fact))
+
