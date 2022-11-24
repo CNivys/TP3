2e2fce70446c1e1a456c8558dd98621cef1d42ac
CNivys <clement.frimin@uha.fr>
2022-11-24 11:58:09 +0100

    correction

--- Exercice 5/Location_vélo.py
+++ Exercice 5/Location_vélo.py
@@ -1,28 +1,36 @@
-hdeb = int(input("Donnez l'heure de début de la location (un entier) : "))
-hfin = int(input("Donnez l'heure de fin de la location (un entier) : "))
+hdeb = 0
+hfin = 0
 j = 0
 h = 0
 phaut = 0
-if 0 <= hdeb < 24 and 0 < hfin < 24:
+fin = False
+while not fin:
+    hdeb = int(input("Donnez l'heure de début de la location (un entier) : "))
+    hfin = int(input("Donnez l'heure de fin de la location (un entier) : "))
     if hdeb == hfin:
         print(" Attention ! l’heure de fin est identique à l’heure de début.")
-    if hdeb > hfin:
+    elif hdeb > hfin:
         print("Attention ! le début de la location est après la fin ...")
-    htotal = hfin - hdeb
-    for i in range(htotal+1):
-        if hdeb >= 7 and hdeb < 17:
-            hdeb += 1
-            j += 1
-        else:
-            h += 1
-            hdeb = hdeb + 1
-else:
-    print("Les heures doivent être comprises entre 0 et 24 !")
+    elif 0 <= hdeb < 24 and 0 <= hfin < 24:
+        htotal = hfin - hdeb
+        for i in range(htotal+1):
+            if hdeb >= 7 and hdeb < 17:
+                hdeb += 1
+                j += 1
+            else:
+                h += 1
+                hdeb = hdeb + 1
+            ab = True
+        if ab == True:
+            h -= 1
+            phaut += 2 * j
+            ptotal = phaut + h
+            print("Vous avez loué votre vélo pendant")
+            print("{} heure(s) au tarif horaire de 1.0 euro(s)".format(h))
+            print("{} heure(s) au tarif horaire de 2.0 euro(s)".format(j))
+            print("Le montant total à payer est de {} euro(s).".format(ptotal))
+            fin = True
+    else:
+        print("Les heures doivent être comprises entre 0 et 24 !")
 
-h-=1
-phaut += 2 * j
-ptotal = phaut + h
-print("Vous avez loué votre vélo pendant")
-print("{} heure(s) au tarif horaire de 1.0 euro(s)".format(h))
-print("{} heure(s) au tarif horaire de 2.0 euro(s)".format(j))
-print("Le montant total à payer est de {} euro(s).".format(ptotal))
+
